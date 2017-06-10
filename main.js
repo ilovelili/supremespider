e24e9f698337b5d2bf87a0901072c271e28274d6
mju <mju@cimpress.com>
2017-06-10 16:46:31 +0900

    working version

--- main.js
+++ main.js
@@ -28,9 +28,7 @@ var casper = require('casper').create(),
     budget = getBudget(),
     utils = require('utils'),
     url = 'http://www.supremenewyork.com/shop',
-    checkouturl = 'http://www.supremenewyork.com/checkout',
-    links = [],
-    ordereditems = [];
+    links = [];
 
 // step 1: open url
 casper.userAgent(config.useragent);
@@ -76,11 +74,8 @@ casper.then(function () {
     console.log('links are:');
     utils.dump(links);
 
-    links.forEach(function (link) {
+    links.forEach(function (link, index, arr) {
         casper.thenOpen('http://www.supremenewyork.com' + link, function () {
-            // first, capture snapshot
-            this.capture(fs.pathJoin('./snapshots', dateservice.today + link + '.png'));
-
             // second, order it
             this.waitForSelector(".sold-out",
                 function soldOut() {
@@ -102,7 +97,6 @@ casper.then(function () {
                             return budget;
                         }
 
-                        /* comment out for testing
                         // select color
                         var coloroption = $('a[data-style-name="' + config.rule.color + '"]');
                         // no that color
@@ -121,13 +115,13 @@ casper.then(function () {
                         $("select[name='size'] option").filter(function () {
                             return $(this).text() == config.rule.size;
                         }).prop('selected', true);
-                        */
+                        $("select[name='size']").trigger('change');
 
                         // select quantity
                         // OK to have no qunatity
                         var quantityoption = $('select[name="qty"]');
                         if (!quantityoption || !quantityoption.length) {
-                            console.info('Qunatity not found. Continue with default quantity ' + config.rule.maxquantity);
+                            console.info('Qunatity not found. Try with default quantity ' + config.rule.maxquantity);
                         } else {
                             var qty = parseInt(config.rule.maxquantity);
                             // invalid qty
@@ -145,31 +139,90 @@ casper.then(function () {
                         }
 
                         // add to cart
-                        $('input[value="カートに入れる"]').click();
-
-                        // console.info(phantom.cookies);
-
-                         // fs.write(cookiefile, JSON.stringify(phantom.cookies));
-
-                        // adjust the current budget
+                        $('input[type="submit"]').click();
                         return budget - price;
                     },
                         {
                             config: config,
-                            budget: budget,                            
+                            budget: budget,
                         }
                     );
 
+                    // view summary
+                    this.then(function () {
+                        this.wait(2000, function () {
+                            // !! last index => check out
+                            if (index == links.length - 1) {
+                                this.evaluate(function summary() {
+                                    //  $('.edit')[0] instead of $('.edit') for anchor click
+                                    $('.edit')[0].click();
+                                });
+                            }
+                        }
+                        );
+                    });
+
+                    // click checkout
+                    this.then(function () {
+                        this.wait(2000, function () {
+                            this.capture(fs.pathJoin('./snapshots', dateservice.today + '/summary.png'));
+                            // then checkout
+                            this.evaluate(function checkout() {
+                                $('.checkout')[0].click();
+                            });
+                        }
+                        );
+                    });
+
+                    // go checkout
+                    this.then(function () {
+                        this.wait(2000, function () {
+                            // fill in the checkout form                                
+                            this.evaluate(function checkout(config) {
+
+                                // do not use $("#order_billing_state).val(' ' + config.userinfo.state) in case select change event not triggered
+                                $("#order_billing_state option").filter(function () {
+                                    return $(this).text().indexOf(config.userinfo.state) > -1; // there is a space prefix on the site...
+                                }).prop('selected', true);
+                                // trigger change manually
+                                $('#order_billing_state').trigger('change');
+
+                                $('#credit_card_type option').filter(function () {
+                                    return $(this).text() == config.payment;
+                                }).prop('selected', true);
+                                // trigger change manually
+                                $('#credit_card_type').trigger('change');
+
+                                $('#credit_card_last_name').val(config.userinfo.lastname);
+                                $('#credit_card_first_name').val(config.userinfo.firstname);
+                                $('#order_email').val(config.userinfo.email);
+                                $('#order_tel').val(config.userinfo.tel);
+                                $('#order_billing_state').val(' ' + config.userinfo.state);
+                                $('#order_billing_city').val(config.userinfo.city);
+                                $('#order_billing_address').val(config.userinfo.street);
+                                $('#order_billing_zip').val(config.userinfo.postalcode);
+
+                                // service terms
+                                $('#order_terms')[0].click();
+
+                                // comment out me to place REAL orders!                                
+                                // $('.checkout')[0].click();
+                            },
+                                {
+                                    config: config
+                                });
+
+                            this.capture(fs.pathJoin('./snapshots', dateservice.today + '/checkout.png'));
+                        }
+                        );
+                    });
+
                     console.log('current budget is ' + budget);
+
                 }, 2000);
         });
     });
 });
-
-// checkout!
-// casper.thenOpen('https://www.supremenewyork.com/checkout', function () {
-//     this.capture(fs.pathJoin('./snapshots', dateservice.today, 'checkout.png'));
-// });
 
 // entry
 casper.run(function () {
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-# TBD
+# Supreme automated order placer
+
+## Dependency
+* [casperjs - browser navigation scripting library](http://casperjs.org/)
+
+## Usage
+* Assign your user info and payment type and order rule in config.json
+* run `casperjs main.js`
